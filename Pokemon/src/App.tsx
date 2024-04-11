4bab46e7918b2ef7e9a7de2b56abc740186abf27
LloydElery <dennisgooglar@gmail.com>
2024-04-11 11:28:21 +0200

    .

--- Pokemon/src/App.tsx
+++ Pokemon/src/App.tsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [pokemon, setPokemon] = useState("pikachu");
   const [pokemonData, setPokemonData] = useState([]);
 
-  const getPokemon = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
+  const inputRef = useRef<HTMLInputElement | null>(null); // korrekt
 
-    const toArray: any = [];
-    toArray.push(result);
-    setPokemonData(toArray);
+  const getPokemon = async () => {
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const response = await fetch(url);
+      const result = await response.json();
+      console.log(result);
+
+      const toArray: any = [];
+      toArray.push(result, ...pokemonData); // ...pokemonData sparar tidigare sökvärden
+      setPokemonData(toArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = () => {
+    setPokemon(inputRef.current!.value); // med ! garanterar vi att den har ett värde
   };
 
   useEffect(() => {
@@ -22,6 +32,9 @@ function App() {
 
   return (
     <>
+      <input ref={inputRef} defaultValue={"Pikachu"}></input>
+      <br />
+      <button onClick={() => handleClick()}>I choose you</button>
       {pokemonData.map((pokemon: any) => (
         <div key={pokemon.id}>
           <img src={pokemon.sprites.front_default}></img>
